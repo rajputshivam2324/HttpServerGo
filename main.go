ad475de91431e0fab2da511c4d21a1975b3baae3
Shivam Kumar <shivam.kumarsingh2324@gmail.com>
2026-01-06 11:43:14 +0530

    Ch1 HTTP Stream Completed

--- main.go
+++ main.go
@@ -5,7 +5,43 @@ import (
 	"os"
 	"log"
 	"bytes"
+	"io"
 )
+
+func getLinesChannel(f io.ReadCloser)<-chan string{
+	out := make(chan string,1)
+
+	go func(){
+		defer f.Close()
+		defer close(out)
+		str:=""
+
+		for{
+			data := make([]byte,8)
+			n,err:=f.Read(data)
+			if err !=nil{
+				break
+			}
+			data=data[:n]
+			if i:=bytes.IndexByte(data,'\n');i!=-1{
+				str+=string(data[:i])
+				data=data[i+1:]
+				out<-str
+				str=""
+			}
+			str+=string(data)
+			if len(str)!=0{
+				out<-str
+			}
+		}
+	}()
+    
+	
+
+	return out
+}
+
+
 
 func main(){
 	f,err :=os.Open("message.txt")
@@ -13,25 +49,12 @@ func main(){
 	if err != nil{
 		log.Fatal("error","error",err)
 	}
-     
-	str:=""
-	for{
-		data := make([]byte,8)
-		n,err :=f.Read(data)
-		if  err != nil{
-			break
-		}
-        data = data[:n]
-		if i:=bytes.IndexByte(data,'\n');i!=-1{
-			str += string(data[:i])
-			data =data[i+1:]
-			fmt.Printf("read: %s\n",str)
-			str=""
-		}
-		str+=string(data)
-		if len(str) !=0{
-			fmt.Printf("read: %s\n",str)
-		}
+
+	lines:=getLinesChannel(f)
+
+	for line := range lines{
+		fmt.Printf("read: %s\n",line)
 	}
+     
 }
 
